a6a49442c98b1dda69a1252b4c04247192f27b26
zhanghuaimin <308765370@qq.com>
2024-11-04 16:19:48 +0800

    一些bug

--- assets/JS/indexJS.js
+++ assets/JS/indexJS.js
@@ -1,7 +1,13 @@
 //登录框调出与使用
+function handleLogin() {
+    // 隐藏登录框
+    hideLoginBox();
+    // 跳转到控制面板页面
+    window.location.href = './Scene/ControlPanel.html';
+}
 
 function showLoginBox() {
-    document.getElementById('loginBox').style.display = 'block';
+    document.getElementById('loginBox').style.display = 'flex';
 }
 
 function hideLoginBox() {
