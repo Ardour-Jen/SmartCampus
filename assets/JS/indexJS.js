e4c9bed8066f6972ca06b5a97c06e5ec850a6f74
Ardour <jiaochenxiang0807@163.com>
2024-11-04 13:56:17 +0800

    11

---
+++ assets/JS/indexJS.js
@@ -0,0 +1,11 @@
+//登录框调出与使用
+
+function showLoginBox() {
+    document.getElementById('loginBox').style.display = 'block';
+}
+
+function hideLoginBox() {
+    document.getElementById('loginBox').style.display = 'none';
+}
+
+
